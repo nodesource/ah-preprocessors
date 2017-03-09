0668573a5d9ea1c0034ac7772fc67948d5ee7ec7
Thorsten Lorenz <thlorenz@gmx.de>
2017-03-09 16:28:41 +0100

    core: scout-function, cloner and resource processor
    - pulled out of ah-net, slightly adapted and documented

---
+++ README.md
@@ -0,0 +1,14 @@
+# ah-preprocessors
+
+Preprocessors of data provided by ah-collector.
+
+## Installation
+
+    npm install ah-preprocessors
+
+## [API](https://nodesource.github.io/ah-preprocessors)
+
+
+## License
+
+MIT
---
+++ ah-preprocessors.js
@@ -0,0 +1,7 @@
+const Cloner = require('./lib/cloner')
+const ResourceProcessor = require('./lib/resource-processor')
+const scoutFunctions = require('./lib/scout-functions')
+
+module.exports = {
+  Cloner, ResourceProcessor, scoutFunctions
+}
